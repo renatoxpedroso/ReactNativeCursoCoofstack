a9956a5b04cf48533d0f125e2c994e73e13ead71
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2023-06-26 20:37:23 -0300

    Criação ActivityIndicator

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import {ActivityIndicator} from 'react-native';
 import {Text} from '../Text/Text';
 import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
 import { buttonPresets } from './buttonPresets';
+import { ActivityIndicator } from '../ActivityIndicator/ActivityIndicator';
 
 export type ButtonPreset = 'primary' | 'outline' | 'secondary';
 interface ButtonPros extends TouchableOpacityBoxProps {
@@ -29,7 +29,7 @@ export function Button({
       {...buttonPreset.container}
       {...touchableOpacityBoxProps}>
       {loading ? (
-        <ActivityIndicator />
+        <ActivityIndicator size={40} color={buttonPreset.content}/>
       ) : (
         <Text preset="paragraphMedium" bold color={buttonPreset.content}>
           {title}
