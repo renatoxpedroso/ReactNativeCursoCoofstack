4bebe57ac2e0f2d28792d94e0cb99fe7af827b4e
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2023-09-13 20:16:32 -0300

    Commit toast

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 import { useToast, useToastService } from '@services';
 import { ToastContent } from './components/ToastContent';
+import { Animated } from 'react-native';
 
 const DEFAULT_DURATION = 2000;
 
@@ -9,16 +10,44 @@ export function Toast() {
   const toast = useToast();
   const { hideToast } = useToastService();
 
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const runEnteringAnimation = useCallback(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
+  const runExitingAnimation = useCallback(
+    (callback: Animated.EndCallback) => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(callback);
+    },
+    [fadeAnim]
+  );
+
   useEffect(() => {
     if (toast) {
+      runEnteringAnimation();
+
       setTimeout(() => {
-        hideToast();
+        runExitingAnimation(hideToast);
       }, toast.duration || DEFAULT_DURATION);
     }
-  }, [hideToast, toast]);
+  }, [hideToast, runEnteringAnimation, runExitingAnimation, toast]);
 
   if (!toast) {
     return null;
   }
-  return <ToastContent toast={toast} />;
+
+  return (
+    <Animated.View style={{ opacity: fadeAnim }}>
+      <ToastContent toast={toast} />
+    </Animated.View>
+  );
 }
