5e66c4fff9962fba5677ae0e8f43dd37b467d72b
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2023-10-18 20:34:46 -0300

    Commit Persistencia de dados com Storage e Zustend

--- src/services/authCredentials/useAuthCredentials.ts
+++ src/services/authCredentials/useAuthCredentials.ts
@@ -1,17 +1,28 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
 import { AuthCredentialsService } from './authCredentialsType';
+import { storage } from '../storage';
 
 export function useAuthCredentials(): AuthCredentialsService {
   return useAuthCredentialsZustand();
 }
 
-const useAuthCredentialsZustand = create<AuthCredentialsService>((set) => ({
-  authCredentials: null,
-  saveCredentias: async (authCredentials) => {
-    set({ authCredentials });
-  },
-  removeCredentials: async () => {
-    set({ authCredentials: null });
-  },
-  isLoading: false,
-}));
+const useAuthCredentialsZustand = create<AuthCredentialsService>()(
+  persist(
+    (set) => ({
+      authCredentials: null,
+      saveCredentias: async (authCredentials) => {
+        set({ authCredentials });
+      },
+      removeCredentials: async () => {
+        set({ authCredentials: null });
+      },
+      isLoading: false,
+    }),
+    {
+      name: '@Atuh',
+      storage: storage,
+    }
+  )
+);
