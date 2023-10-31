9920e84cd7912ac16e2508db390d0c86eb9effc1
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2023-10-30 21:14:33 -0300

    Commit storage MMKV

--- App.tsx
+++ App.tsx
@@ -5,9 +5,11 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Router } from './src/routes/Routes';
 import { Toast } from '@components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthCredentialsProvider } from '@services';
+import { AuthCredentialsProvider, MMKVStorage, initiaalizeStorage } from '@services';
 
 const queryClient = new QueryClient();
+
+initiaalizeStorage(MMKVStorage);
 
 function App(): JSX.Element {
   return (
@@ -15,12 +17,8 @@ function App(): JSX.Element {
       <QueryClientProvider client={queryClient}>
         <SafeAreaProvider>
           <ThemeProvider theme={theme}>
-            {/* Only use ToastProvider if it is using Context implementation.
-  
-            {/* <ToastProvider> */}
             <Router />
             <Toast />
-            {/* </ToastProvider> */}
           </ThemeProvider>
         </SafeAreaProvider>
       </QueryClientProvider>
--- src/services/storage/storage.ts
+++ src/services/storage/storage.ts
@@ -1,9 +1,14 @@
-import { asyncStorage } from './implementation/asyncStorage';
-
 export interface Storage {
   getItem: <T = unknown>(key: string) => Promise<T | null>;
   setItem: <T>(key: string, value: T) => Promise<void>;
   removeItem: (key: string) => Promise<void>;
 }
 
-export let storage: Storage = asyncStorage;
+//export let storage: Storage = asyncStorage;
+//export let storage: Storage = MMKVStorage;
+
+export let storage: Storage;
+
+export function initiaalizeStorage(storageImpl: Storage) {
+  storage = storageImpl;
+}
