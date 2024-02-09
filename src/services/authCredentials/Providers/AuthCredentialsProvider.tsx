04f3e0f3e198afafe550d24f87f0b1415fb5c253
Renato Xavier Pedroso <renatoxpedroso@gmail.com>
2024-02-09 20:51:45 -0300

    Fluxo de Autorização com Interceptor

--- src/services/authCredentials/Providers/AuthCredentialsProvider.tsx
+++ src/services/authCredentials/Providers/AuthCredentialsProvider.tsx
@@ -5,7 +5,7 @@ import { AuthCredentials, authService } from '@domain';
 
 import { AuthCredentialsService } from '../authCredentialsType';
 import { authCredentialsStorage } from '../authCredentialsStorage';
-import { ca, tr } from 'date-fns/locale';
+import { api } from '@api';
 
 export const AuthCredentialsContext = createContext<AuthCredentialsService>({
   authCredentials: null,
@@ -21,6 +21,34 @@ export function AuthCredentialsProvider({ children }: React.PropsWithChildren<{}
   useEffect(() => {
     startAuthCredentials();
   }, []);
+
+  useEffect(() => {
+    const interceptor = api.interceptors.response.use(
+      (response) => response,
+      async (responseError) => {
+        if (responseError.response.status === 401) {
+          if (!authCredentials?.refreshToken) {
+            removeCredentials();
+            return Promise.reject(responseError);
+          }
+
+          const failedRequest = responseError.config;
+
+          const newAuthCredentials = await authService.authenticateByRefreshToken(
+            authCredentials?.refreshToken
+          );
+          saveCredentials(newAuthCredentials);
+
+          failedRequest.headers.Authorization = `Bearer ${newAuthCredentials.token}`;
+
+          return api(failedRequest);
+        }
+      }
+    );
+
+    // remove listener when component unmount
+    return () => api.interceptors.response.eject(interceptor);
+  }, [authCredentials?.refreshToken]);
 
   async function startAuthCredentials() {
     try {
